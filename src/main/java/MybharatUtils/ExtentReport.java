4ace2e9587bd903a8bbe64df1f2a6d1f4adac8ed
kumarshouryap <kumarshouryap@gmail.com>
2026-01-08 21:32:42 +0530

    extentreport

---
+++ src/main/java/MybharatUtils/ExtentReport.java
@@ -0,0 +1,25 @@
+package MybharatUtils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReport {
+
+	private static ExtentReports extentreport;
+
+	public static ExtentReports getExtentReport() {
+
+		if (extentreport == null) {
+			String reportpath = System.getProperty("user.dir") + "/reports/extentreports.html";
+			ExtentSparkReporter sparkreporter = new ExtentSparkReporter(reportpath);
+			sparkreporter.config().setReportName("Mybharat Automation Test Report");
+			sparkreporter.config().setDocumentTitle("Test Results of Mybharat Application");
+			extentreport = new ExtentReports();
+			extentreport.attachReporter(sparkreporter);
+			extentreport.setSystemInfo("Tester", "Manoj Kumar");
+
+		}
+		return extentreport;
+
+	}
+}
--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -1,52 +1,64 @@
 package TestComponents;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentReporter;
 import com.microsoft.playwright.*;
 
+import MybharatUtils.ExtentReport;
 import MybharatUtils.Log;
 import Pageobjects.LandingPage;
 
 public class BaseTest {
 
-    protected Playwright playwright;
-    protected Browser browser;
-    protected BrowserContext context;
-    protected Page page;
-    protected LandingPage landingPage;
+	protected Playwright playwright;
+	protected Browser browser;
+	protected BrowserContext context;
+	protected Page page;
+	protected LandingPage landingPage;
+	protected static ExtentReports extentreport;
 
-    @BeforeClass
-    public void initializeBrowser() {
-    	 Log.info("---------------Create Playwright instance ------------");
-        playwright = Playwright.create();
-        
-      	 Log.info("---------------Launching Chromium browser with Chrome channel ------------");
+	@BeforeSuite
 
-        browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions()
-                        .setChannel("chrome")
-                        .setHeadless(false));
-        Log.info("---------------Browser Screen Size 1920, 1080 opening successfully ------------");
+	public void SetupExtentReport() {
+		extentreport = ExtentReport.getExtentReport();
+	}
 
-        context = browser.newContext(
-                new Browser.NewContextOptions()
-                        .setViewportSize(1920, 1080));
-        
-        
-        Log.info("---------------Opening new Browser Page/Tab ------------");
-        page = context.newPage();
-        
-        Log.info("---------------Create Landing Page Object Successfullay ------------");
-        
-        landingPage = new LandingPage(page);
-    }
+	@BeforeClass
+	public void initializeBrowser() {
+		Log.info("---------------Create Playwright instance ------------");
+		playwright = Playwright.create();
 
-    @AfterClass
-    public void tearDown() {
-    	
-    	Log.info("---------------Close Playwright instance Successfullay ------------");
-    	
-        playwright.close();
-    }
+		Log.info("---------------Launching Chromium browser with Chrome channel ------------");
+
+		browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+		Log.info("---------------Browser Screen Size 1920, 1080 opening successfully ------------");
+
+		context = browser.newContext(new Browser.NewContextOptions().setViewportSize(1920, 1080));
+
+		Log.info("---------------Opening new Browser Page/Tab ------------");
+		page = context.newPage();
+
+		Log.info("---------------Create Landing Page Object Successfullay ------------");
+
+		landingPage = new LandingPage(page);
+	}
+
+	@AfterClass
+	public void tearDown() {
+
+		Log.info("---------------Close Playwright instance Successfullay ------------");
+
+		playwright.close();
+	}
+
+	@AfterSuite
+
+	public void flushReport() {
+		extentreport.flush();
+	}
 }
